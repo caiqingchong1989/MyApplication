2d6e0d0197c8f47a4f319a37fdebe7ddece01fe5
huangshilei@vipkid.com <huangshilei@vipkid.com>
2021-01-14 19:02:27 +0800

    添加toast

--- app/src/main/java/com/shilei/myapplication/MainActivity.java
+++ app/src/main/java/com/shilei/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.shilei.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.textview).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"hello world",Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
